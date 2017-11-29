97fd1e0dd7bdc80bad5281200ddd0d92011bc1db
Joel Petersson <joel.anders.petersson@gmail.com>
2017-11-29 23:39:08 +0100

    Fixed error with swapped bytes in 3 byte instructions.

--- 8080_disassembler/Intel_8080_Disassembler.c
+++ 8080_disassembler/Intel_8080_Disassembler.c
@@ -5,7 +5,7 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
     //Switch for checking op codes
     switch (codeBuffer[pc]) {
       case 0x00: printf("%04X   NOP\n",pc); break;
-      case 0x01: printf("%04X   LXI  B   #$%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0x01: printf("%04X   LXI  B   #$%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0x02: printf("%04X   STAX B\n",pc); break;
       case 0x03: printf("%04X   INX  B\n",pc); break;
       case 0x04: printf("%04X   INR  B\n",pc); break;
@@ -22,7 +22,7 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
       case 0x0F: printf("%04X   RRC\n",pc); break;
 
       case 0x10: printf("%04X   NOP*\n",pc); break;
-      case 0x11: printf("%04X   LXI  D   #$%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0x11: printf("%04X   LXI  D   #$%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0x12: printf("%04X   STAX D\n",pc); break;
       case 0x13: printf("%04X   INX  D\n",pc); break;
       case 0x14: printf("%04X   INR  D\n",pc); break;
@@ -39,8 +39,8 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
       case 0x1F: printf("%04X   RAR\n",pc); break;
 
       case 0x20: printf("%04X   NOP*\n",pc); break;
-      case 0x21: printf("%04X   LXI  H   #$%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0x22: printf("%04X   SHLD      $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0x21: printf("%04X   LXI  H   #$%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0x22: printf("%04X   SHLD      $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0x23: printf("%04X   INX  H\n",pc); break;
       case 0x24: printf("%04X   INR  H\n",pc); break;
       case 0x25: printf("%04X   DCR  H\n",pc); break;
@@ -48,7 +48,7 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
       case 0x27: printf("%04X   DAA\n",pc); break;
       case 0x28: printf("%04X   NOP*\n",pc); break;
       case 0x29: printf("%04X   DAD  SP\n",pc); break;
-      case 0x2A: printf("%04X   LHLD      $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0x2A: printf("%04X   LHLD      $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0x2B: printf("%04X   DCX  H\n",pc); break;
       case 0x2C: printf("%04X   INR  L\n",pc); break;
       case 0x2D: printf("%04X   DCR  L\n",pc); break;
@@ -56,8 +56,8 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
       case 0x2F: printf("%04X   CMA\n",pc); break;
 
       case 0x30: printf("%04X   NOP*\n",pc); break;
-      case 0x31: printf("%04X   LXI  SP  #$%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0x32: printf("%04X   STA       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0x31: printf("%04X   LXI  SP  #$%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0x32: printf("%04X   STA       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0x33: printf("%04X   INX  SP\n",pc); break;
       case 0x34: printf("%04X   INR  M\n",pc); break;
       case 0x35: printf("%04X   DCR  M\n",pc); break;
@@ -65,7 +65,7 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
       case 0x37: printf("%04X   STC\n",pc); break;
       case 0x38: printf("%04X   NOP*\n",pc); break;
       case 0x39: printf("%04X   DAD  SP\n",pc); break;
-      case 0x3A: printf("%04X   LDA       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0x3A: printf("%04X   LDA       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0x3B: printf("%04X   DCX  SP\n",pc); break;
       case 0x3C: printf("%04X   INR  A\n",pc); break;
       case 0x3D: printf("%04X   DCR  A\n",pc); break;
@@ -210,69 +210,69 @@ void dissasemble8080_ROM(unsigned char *codeBuffer, int arraySize) {
 
       case 0xC0: printf("%04X   RNZ\n",pc); break;
       case 0xC1: printf("%04X   POP  B\n",pc); break;
-      case 0xC2: printf("%04X   JNZ       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xC3: printf("%04X   JMP       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xC4: printf("%04X   CNZ       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xC2: printf("%04X   JNZ       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xC3: printf("%04X   JMP       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xC4: printf("%04X   CNZ       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xC5: printf("%04X   PUSH B\n",pc); break;
       case 0xC6: printf("%04X   ADI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xC7: printf("%04X   RST  0\n",pc); break;
       case 0xC8: printf("%04X   RZ\n",pc); break;
       case 0xC9: printf("%04X   RET\n",pc); break;
-      case 0xCA: printf("%04X   JZ        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xCB: printf("%04X   JMP*      $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xCC: printf("%04X   CZ        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xCD: printf("%04X   CALL      $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xCA: printf("%04X   JZ        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xCB: printf("%04X   JMP*      $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xCC: printf("%04X   CZ        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xCD: printf("%04X   CALL      $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xCE: printf("%04X   ACI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xCF: printf("%04X   RST  1\n",pc); break;
 
       case 0xD0: printf("%04X   RNC\n",pc); break;
       case 0xD1: printf("%04X   POP  D\n",pc); break;
-      case 0xD2: printf("%04X   JNC       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xD2: printf("%04X   JNC       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xD3: printf("%04X   OUT      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
-      case 0xD4: printf("%04X   CNC       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xD4: printf("%04X   CNC       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xD5: printf("%04X   PUSH D\n",pc); break;
       case 0xD6: printf("%04X   SUI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xD7: printf("%04X   RST  2\n",pc); break;
       case 0xD8: printf("%04X   RC\n",pc); break;
       case 0xD9: printf("%04X   RET*\n",pc); break;
-      case 0xDA: printf("%04X   JC        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xDA: printf("%04X   JC        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xDB: printf("%04X   IN       #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
-      case 0xDC: printf("%04X   CC        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xDD: printf("%04X   CALL*     $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xDC: printf("%04X   CC        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xDD: printf("%04X   CALL*     $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xDE: printf("%04X   SBI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xDF: printf("%04X   RST  3\n",pc); break;
 
       case 0xE0: printf("%04X   RPO\n",pc); break;
       case 0xE1: printf("%04X   POP  H\n",pc); break;
-      case 0xE2: printf("%04X   JPO       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xE2: printf("%04X   JPO       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xE3: printf("%04X   XTHL\n",pc); break;
-      case 0xE4: printf("%04X   CPO       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xE4: printf("%04X   CPO       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xE5: printf("%04X   PUSH H\n",pc); break;
       case 0xE6: printf("%04X   ANI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xE7: printf("%04X   RST  4\n",pc); break;
       case 0xE8: printf("%04X   RPE\n",pc); break;
       case 0xE9: printf("%04X   PCHL\n",pc); break;
-      case 0xEA: printf("%04X   JPE       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xEA: printf("%04X   JPE       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xEB: printf("%04X   XCHG\n",pc); break;
-      case 0xEC: printf("%04X   CPE       $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xED: printf("%04X   CALL*     $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xEC: printf("%04X   CPE       $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xED: printf("%04X   CALL*     $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xEE: printf("%04X   XRI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xEF: printf("%04X   RST  5\n",pc); break;
 
       case 0xF0: printf("%04X   RP\n",pc); break;
       case 0xF1: printf("%04X   POP  PSW\n",pc); break;
-      case 0xF2: printf("%04X   JP        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xF2: printf("%04X   JP        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xF3: printf("%04X   DI\n",pc); break;
-      case 0xF4: printf("%04X   CP        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xF4: printf("%04X   CP        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xF5: printf("%04X   PUSH PSW\n",pc); break;
       case 0xF6: printf("%04X   ORI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xF7: printf("%04X   RST  6\n",pc); break;
       case 0xF8: printf("%04X   RM\n",pc); break;
       case 0xF9: printf("%04X   SPHL\n",pc); break;
-      case 0xFA: printf("%04X   JM        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xFA: printf("%04X   JM        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xFB: printf("%04X   EI\n",pc); break;
-      case 0xFC: printf("%04X   CM        $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
-      case 0xFD: printf("%04X   CALL*     $%02X%02X\n", pc, codeBuffer[pc+1], codeBuffer[pc+2]); pc = pc +2; break;
+      case 0xFC: printf("%04X   CM        $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
+      case 0xFD: printf("%04X   CALL*     $%02X%02X\n", pc, codeBuffer[pc+2], codeBuffer[pc+1]); pc = pc +2; break;
       case 0xFE: printf("%04X   CPI      #$%02X\n",pc, codeBuffer[pc+1]); pc++; break;
       case 0xFF: printf("%04X   RST  7\n",pc); break;
 
@@ -306,7 +306,7 @@ int main() {
   FILE *fp;
   //invaders.h path : C://Users/Joel/Documents/Code/Space_invaders_emu/ROMs/invaders.h
   // Open ROM and fill array with its contents
-  fp = fopen("C://Users/Joel/Documents/Code/Space_invaders_emu/ROMs/invaders.h", "rb");
+  fp = fopen("C://Users/Joel/Documents/Code/Space_invaders_emu/ROMs/invaders", "rb");
   fseek(fp, 0, SEEK_END);
   int sizeOfFile = ftell(fp);
   rewind(fp);
@@ -318,7 +318,7 @@ int main() {
 
 
 
-  print_code_in_hex(buffer, sizeOfFile);
+  //print_code_in_hex(buffer, sizeOfFile);
   printf("\n\nSize: %d bytes\n\n",sizeOfFile);
   dissasemble8080_ROM(buffer, sizeOfFile);
 
